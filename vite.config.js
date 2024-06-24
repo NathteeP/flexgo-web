9d25647cff52dcbf9c4f3f82d451c6e4154f959c
minipongpang <keartisuksookkow@gmail.com>
2024-06-24 17:07:18 +0700

    test Component Button and Input

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin({ fix: true })],
+  plugins: [react()],
   base: '/',
   define: {
     'process.env': process.env,
