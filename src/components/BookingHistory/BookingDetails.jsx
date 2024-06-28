415ce92416567a6d22097bbc270761a79c9ef0a5
nattawanoyster <nattawan.prs@gmail.com>
2024-06-28 14:29:44 +0700

    user booking history page  done

---
+++ src/components/BookingHistory/BookingDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const BookingDetail = ({ booking, status }) => (
+  <div className='p-4 mb-4 bg-fg-primary-03 shadow-md rounded-xl'>
+    <h2 className='text-xl font-semibold'>{booking.hotel}</h2>
+    <div class='grid grid-cols-3 gap-4 mt-2'>
+      <p className='text-fg-text-black font-medium'>{booking.room}</p>
+      <p className='text-fg-text-black'>{booking.guests}</p>
+      <p className='text-fg-text-black'>{booking.dates}</p>
+    </div>
+    <div className='flex justify-end mt-6'>
+      <p
+        className={status === 'Pending' ? 'text-yellow-500' : 'text-green-500'}
+      >
+        {status}
+      </p>
+    </div>
+  </div>
+);
+
+export default BookingDetail;
