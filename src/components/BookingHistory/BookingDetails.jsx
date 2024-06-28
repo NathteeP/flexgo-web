e0464942f0b6a4ddbb0197f05aed6b1f4b05f0a6
nattawanoyster <nattawan.prs@gmail.com>
2024-06-28 17:31:45 +0700

    user-reservation-page done / booking-detail-page fixed

--- src/components/BookingHistory/BookingDetails.jsx
+++ src/components/BookingHistory/BookingDetails.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const BookingDetail = ({ booking, status }) => (
   <div className='p-4 mb-4 bg-fg-primary-03 shadow-md rounded-xl'>
     <h2 className='text-xl font-semibold'>{booking.hotel}</h2>
-    <div class='grid grid-cols-3 gap-4 mt-2'>
+    <h2 className='text-lg font-light text-fg-text-black text-opacity-70'>
+      {' '}
+      Reservation ID :{booking.reservationID}
+    </h2>
+    <div class='grid grid-cols-3 gap-4 mt-4'>
       <p className='text-fg-text-black font-medium'>{booking.room}</p>
       <p className='text-fg-text-black'>{booking.guests}</p>
       <p className='text-fg-text-black'>{booking.dates}</p>
---
+++ src/pages/Users/BookingReservation.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import UserDetails from '../../components/ReservationDetail/UserDetail';
+import HotelDetails from '../../components/ReservationDetail/HotelDeatials';
+import TitlePage from '../../layouts/TitlePage';
+
+const userInfo = {
+  name: 'Katarina Bluu',
+  userId: '11042000',
+  hotelName: 'Novotel Bangkok IMPACT',
+  address:
+    '94 Popular Road, Banmai Sub District, Pakkred, 11120 Nonthaburi, Thailand',
+};
+
+const HotelInfo = {
+  adults: 2,
+  roomType: 'Standard Twin Room',
+  nights: 1,
+  checkInDate: { day: 28, month: 'September' },
+  checkOutDate: { day: 29, month: 'September' },
+  price: '199',
+  tax: '20',
+  totalPrice: '219',
+  imageSrc:
+    'https://cf2.bstatic.com/xdata/images/hotel/max1024x768/252156497.jpg?k=355b900708a7de8ebcbae929334c8875ec2fa6e7f0f8cf3f59897a62f18ed883&o=&hp=1',
+};
+
+const BookingReservationPage = () => {
+  return (
+    <div className=' flex flex-col items-center justify-center p-4 mx-16'>
+      {/* <div className='w-full'>
+        <TitlePage> Reservation </TitlePage>
+      </div> */}
+      <div className='max-w-5xl w-full border-[2px] border-fg-grey/50 rounded-2xl flex flex-col items-center p-8 my-24'>
+        <div className='max-w-4xl w-full flex justify-start mb-8'>
+          <p className='font-medium mt-2 text-xl text-fg-text-black'>
+            Reservation ID: 17180820245689
+          </p>
+        </div>
+        <div className='bg-fg-primary-01 bg-opacity-60 shadow-md rounded-tl-2xl rounded-tr-2xl max-w-5xl w-full border-[1px] border-fg-grey/50'>
+          <UserDetails
+            name={userInfo.name}
+            userId={userInfo.userId}
+            hotelName={userInfo.hotelName}
+            address={userInfo.address}
+          />
+        </div>
+        <div className='bg-fg-primary-03 shadow-md rounded-bl-2xl rounded-br-2xl max-w-5xl w-full border-[1px] border-fg-grey/50'>
+          <HotelDetails
+            adults={HotelInfo.adults}
+            roomType={HotelInfo.roomType}
+            nights={HotelInfo.nights}
+            checkInDate={HotelInfo.checkInDate}
+            checkOutDate={HotelInfo.checkOutDate}
+            price={HotelInfo.price}
+            tax={HotelInfo.tax}
+            totalPrice={HotelInfo.totalPrice}
+            imageSrc={HotelInfo.imageSrc}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingReservationPage;
