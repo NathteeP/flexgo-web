7c7da7b1469e81e7196093d91cf4e0f444738cc0
ChampPABA <aimilic@gmail.com>
2024-07-02 00:25:09 +0700

    dropdown need discussion

--- src/layouts/MainContainer.jsx
+++ src/layouts/MainContainer.jsx
@@ -1,8 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAuthUser } from '../store/slices/user-slice';
 
 const MainContainer = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAuthUser());
+  }, [dispatch]);
+
   return (
     <>
       <Header />
