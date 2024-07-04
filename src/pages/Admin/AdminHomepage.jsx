2f4bf3258ffc6381a139c32dfa7d32e8e59e7dac
nattawanoyster <nattawan.prs@gmail.com>
2024-07-04 15:33:10 +0700

    host asset management done, added admin route index page

---
+++ src/pages/Admin/AdminHomepage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AdminHomepage() {
+  return <div>AdminHomepage</div>;
+}
+
+export default AdminHomepage;
---
+++ src/pages/Admin/AdminHostNoti.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AdminHostNoti() {
+  return <div>AdminHostNoti</div>;
+}
+
+export default AdminHostNoti;
