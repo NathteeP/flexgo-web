21816a11ccc357653741f0229180c15c39df5b09
minipongpang <keartisuksookkow@gmail.com>
2024-07-11 14:21:16 +0700

    for PGRIDDDDD

--- src/store/slices/modal-slice.js
+++ src/store/slices/modal-slice.js
@@ -20,6 +20,7 @@ const initialState = {
   isHostNotiByAdminOpen: false,
   isAdminRemoveRoomOpen: false,
   isHostConfirmAddNewAccom: false,
+  isReviewOpen: false,
 };
 
 const modalSlice = createSlice({
@@ -123,6 +124,12 @@ const modalSlice = createSlice({
     closeHostConfirmAddNewAccom: (state) => {
       state.isHostConfirmAddNewAccom = false;
     },
+    openReview: (state) => {
+      state.isReviewOpen = true;
+    },
+    closeReview: (state) => {
+      state.isReviewOpen = false;
+    },
   },
 });
 
@@ -155,5 +162,7 @@ export const {
   closeAdminRemoveRoom,
   openHostConfirmAddNewAccom,
   closeHostConfirmAddNewAccom,
+  openReview,
+  closeReview,
 } = modalSlice.actions;
 export const modalReducer = modalSlice.reducer;
